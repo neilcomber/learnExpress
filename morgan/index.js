6fba3df174463cedb15bb3dbf31c3ff7af63e4d7
Neil Comber <neilcomber@Neils-MacBook-Pro.local>
2024-01-16 09:20:05 +0000

    yet more express

--- morgan/index.js
+++ morgan/index.js
@@ -2,17 +2,49 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express();
 
-app.use(()=>{
-    console.log('making my middleware!!!')
-    next();
-});
+//  app.use(morgan('common'))
+
+// app.use((req, res, next)=>{
+//     console.log('making my middleware!!!')
+//     next();
+//     console.log('call after middleware')
+// });
+// app.use((req, res, next)=>{
+//     console.log('Thisis my 2nd middleware!!!')
+//     next();
+// });
+
+app.use('/dogs', (req, res, next)=>{
+    req.requestTime = Date.now();
+    console.log(req.requestTime)
+return next()
+})
+
+const verifyPassword = (req, res, next) => {
+    if(req.query.password === '1234'){
+        console.log('this is the secret page')
+        next();
+    }
+    else{
+        res.send('you cant know the secret!!!!')
+    }
+   
+}
 
 app.get('/', (req, res) =>{
-    res.send('HOME PAGE')
+    res.send(`HOME PAGE`)
+})
+
+app.get('/secret', verifyPassword, (req, res) =>{
+    res.send(`This is the secret page`)
 })
 
 app.get('/dogs', (req, res) =>{
     res.send('WOOF WOOF')
+})
+
+app.use((req, res)=>{
+    res.send('page not found')
 })
 
 app.listen(3000, ()=>{
