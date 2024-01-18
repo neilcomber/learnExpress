8f6bf92ce4246b1199ad7b71a995a619c3d31380
Neil Comber <neilcomber@Neils-MacBook-Pro.local>
2024-01-18 09:05:17 +0000

    error handling in express

--- morgan/index.js
+++ morgan/index.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express();
+const AppError = require('./AppError');
 
-//  app.use(morgan('common'))
-
-// app.use((req, res, next)=>{
-//     console.log('making my middleware!!!')
-//     next();
-//     console.log('call after middleware')
-// });
-// app.use((req, res, next)=>{
-//     console.log('Thisis my 2nd middleware!!!')
-//     next();
-// });
 
 app.use('/dogs', (req, res, next)=>{
     req.requestTime = Date.now();
@@ -22,17 +12,22 @@ return next()
 
 const verifyPassword = (req, res, next) => {
     if(req.query.password === '1234'){
-        console.log('this is the secret page')
+        
         next();
     }
     else{
-        res.send('you cant know the secret!!!!')
+        res.status(401)
+       throw new AppError('You need a password!', 401)
     }
    
 }
 
 app.get('/', (req, res) =>{
     res.send(`HOME PAGE`)
+})
+
+app.get('/error', (req, res)=> {
+    chicken.fly()
 })
 
 app.get('/secret', verifyPassword, (req, res) =>{
@@ -43,8 +38,19 @@ app.get('/dogs', (req, res) =>{
     res.send('WOOF WOOF')
 })
 
+app.get('/admin', (req, res) =>{
+    throw new AppError('YOU ARE NOT AN ADMIN!!!!', 403)
+})
+
 app.use((req, res)=>{
     res.send('page not found')
+})
+
+app.use((err, req, res, next)=>{
+   const { status = 500 } = err;
+   const { message = 'something went Wrong'} = err;
+   res.status(status).send(message)
+    next()
 })
 
 app.listen(3000, ()=>{
